62a1208b17d7f964b7cdc5a980924c343beeb349
greatshedy <greatboyshedy1@gmail.com>
2026-04-29 19:38:35 +0100

    feat: update api baseURL to support environment variable configuration

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/admin",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/admin",
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
